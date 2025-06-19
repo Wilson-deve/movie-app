872d0226cd2e53dbf17dd903b98c651081f585f4
Wilson-deve <nshimiyimanawilson91@gmail.com>
2025-06-19 20:27:07 +0200

    ui: add heading to main page with gradient text effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+import Search from "./components/Search";
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <main className="pattern">
       <div className="wrapper">
         <header>
+          <img src="./hero.png" alt="Hero Banner" />
           <h1>
-            Find <span className="text-gradient">Movies</span> You'll Enjoy
-            Without the Hassle
+            Discover <span className="text-gradient">Movies</span> You’ll Love
+            Instantly
           </h1>
         </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1>
+          {searchTerm ? `Searching for "${searchTerm}"` : "Popular Movies"}
+        </h1>
       </div>
     </main>
   );
